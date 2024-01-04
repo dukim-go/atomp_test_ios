dedec2023289bf69282889f984183caae252621a
김대웅 <dukim.dev@lgcns.com>
2024-01-04 14:37:42 +0900

    init

---
+++ TestATOMP_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestATOMP_iOS
+//
+//  Created by dukim on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
